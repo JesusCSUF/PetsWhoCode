fb70d9297d9d7f1024a8354881ccab683632ba78
Ivan Lo <loi1@nationwide.com>
2019-07-19 03:18:34 -0700

    bug fix

--- src2/PetVision/PetVision.Web/Controllers/QuoteController.cs
+++ src2/PetVision/PetVision.Web/Controllers/QuoteController.cs
@@ -30,20 +30,35 @@ namespace PetVision.Web.Controllers
             var petState = TempData.Peek("PetState");
             var petZipCode = TempData.Peek("PetZipCode");
             var predictedBreed = prediction.Predictions.OrderByDescending(x => x.Probability).First(x=>x.TagName != "Canine" && x.TagName != "Feline");
-
-           
-
-            petState = "CA";
+            
+            if(predictedBreed.TagName == "Regdoll")
+            {
+                predictedBreed.TagName = "Ragdoll";
+            }
 
             using (var ctx = new ClaimDataContext())
             {
-                var claimData = ctx.Conditions.Where(x => x.Breed == predictedBreed.TagName.ToUpper() && x.State == petState.ToString()).OrderBy(x => x.Rank).Take(3).Select(x => new PetCondition
+                List<PetCondition> claimData = null;
+                try
                 {
-                    ClaimAmount = x.ClaimAmount,
-                    Condition = x.DiagnosisCodeDesc,
-                    PaidAmount = x.PaidAmount,
-                    Rank = x.Rank,
-                }).ToList();
+                    claimData = ctx.Conditions.Where(x => x.Breed == predictedBreed.TagName.ToUpper() && x.State == petState.ToString()).OrderBy(x => x.Rank).Take(3).Select(x => new PetCondition
+                    {
+                        ClaimAmount = x.ClaimAmount,
+                        Condition = x.DiagnosisCodeDesc,
+                        PaidAmount = x.PaidAmount,
+                        Rank = x.Rank,
+                    }).ToList();
+                }
+                catch(Exception err)
+                {
+                    claimData = ctx.Conditions.Where(x => x.Breed == predictedBreed.TagName.ToUpper()).OrderBy(x => x.Rank).Take(3).Select(x => new PetCondition
+                    {
+                        ClaimAmount = x.ClaimAmount,
+                        Condition = x.DiagnosisCodeDesc,
+                        PaidAmount = x.PaidAmount,
+                        Rank = x.Rank,
+                    }).ToList();
+                }
 
                 var petInfos = ctx.PetInfos.First(x => x.Breed == predictedBreed.TagName);
                 
@@ -100,7 +115,7 @@ namespace PetVision.Web.Controllers
                     Pet = quote.Quote.Pets.First(),
                     Pricings = pricingBreakDown,
                     State = petState.ToString(),
-                    ZipCode = petState.ToString(),
+                    ZipCode = petZipCode.ToString(),
                 };
 
                 return View(dataModel);
